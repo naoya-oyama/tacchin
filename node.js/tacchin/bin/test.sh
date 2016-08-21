6a883fa02186f31309cb4073ba717458ced57298
Naoya OYAMA <naoyan@haswell.minaminoshima.org>
2016-08-21 11:50:00 +0900

    2ストリームの同時再生対応

--- node.js/tacchin/bin/test.sh
+++ node.js/tacchin/bin/test.sh
@@ -3,4 +3,6 @@ export LIBVA_DRIVERS_PATH=/opt/intel/mediasdk/lib64
 export LIBVA_DRIVER_NAME=iHD
 export MFX_HOME=/opt/intel/mediasdk
 export PKG_CONFIG_PATH=/opt/intel/opencl:
-( /usr/local/bin/recpt1 --device /dev/$1 --b25 --sid $3 $2 - - | ffmpeg -y -i - -vf yadif=0 -q:v 18 -b:v 8000k -vcodec h264_qsv -vcm 0 -look_ahead 0 -preset veryslow -profile:v main -acodec aac -ar 44100 -f dash -live 1 -min_seg_duration 3000000 -window_size 4 -extra_window_size 2 -remove_at_exit 1 ~/tacchin/data/$1.mpd ) > /tmp/tacchin.log 2>&1
+[ ! -d ~/tacchin/data/$1/ ]  && mkdir -p ~/tacchin/data/$1
+( /usr/local/bin/recpt1 --device /dev/$1 --b25 --sid $3 $2 - - | ffmpeg -y -i - -vf yadif=0 -q:v 18 -b:v 8000k -vcodec h264_qsv -vcm 0 -look_ahead 0 -preset veryslow -profile:v main -acodec aac -ar 44100 -f dash -live 1 -min_seg_duration 3000000 -window_size 4 -extra_window_size 2 -remove_at_exit 1 ~/tacchin/data/$1/$1.mpd ) > /tmp/tacchin.log 2>&1
+rm -fr ~/tacchin/data/$1
